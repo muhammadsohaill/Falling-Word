be43731af70accb0016c00124e820d7eac52d07f
sohail <muhammadsohail299@gmail.com>
2022-02-20 01:53:49 +0500

    Final Commit

--- app/src/main/java/com/babbel/fallingwords/ui/game/StartFragment.kt
+++ app/src/main/java/com/babbel/fallingwords/ui/game/StartFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.babbel.fallingword.R
+import com.babbel.fallingwords.R
 import kotlinx.android.synthetic.main.fragment_start.*
 
 class StartFragment : Fragment() {
